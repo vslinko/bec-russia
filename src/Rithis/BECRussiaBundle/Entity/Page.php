36fd49271805b38f05b7efcc066f09a0852f7d7a
Vyacheslav Slinko <vyacheslav.slinko@gmail.com>
2012-11-09 21:06:33 +0400

    Search

--- src/Rithis/BECRussiaBundle/Entity/Page.php
+++ src/Rithis/BECRussiaBundle/Entity/Page.php
@@ -5,7 +5,7 @@ namespace Rithis\BECRussiaBundle\Entity;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * @ORM\Entity
+ * @ORM\Entity(repositoryClass="Rithis\BECRussiaBundle\Entity\PageRepository")
  * @ORM\Table("pages")
  */
 class Page
