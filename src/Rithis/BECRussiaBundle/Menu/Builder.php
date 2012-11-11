e7e7351f25f536108de84a446222935f81a03e0b
Vyacheslav Slinko <vyacheslav.slinko@gmail.com>
2012-11-12 01:14:51 +0400

    Better menu zen-16

---
+++ src/Rithis/BECRussiaBundle/Menu/Builder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Rithis\BECRussiaBundle\Menu;
+
+use Symfony\Component\DependencyInjection\ContainerAware,
+    Knp\Menu\FactoryInterface;
+
+class Builder extends ContainerAware
+{
+    public function mainMenu(FactoryInterface $factory, array $options)
+    {
+        $menu = $factory->createItem('root');
+
+        $menu->setCurrentUri($this->container->get('request')->getRequestUri());
+
+        $menu->addChild('О центре', array('route' => 'rithis_becrussia_index_get'));
+        $menu->addChild('Школы', array('route' => 'rithis_becrussia_school_all'));
+        $menu->addChild('Вакансии', array('route' => 'rithis_becrussia_vacancy_all'));
+        $menu->addChild('Библиотека', array('route' => 'rithis_becrussia_library_categories'));
+
+        return $menu;
+    }
+}
