ad3f81c012acbdf1d0eebbfeefd651cbb178f71b
Andrey Linko <su2ny@mail.ru>
2012-12-05 02:14:57 +0400

    Add test registration and submit form

--- src/Rithis/BECRussiaBundle/Controller/TestController.php
+++ src/Rithis/BECRussiaBundle/Controller/TestController.php
@@ -6,11 +6,39 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template,
     Sensio\Bundle\FrameworkExtraBundle\Configuration\Method,
     Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
+use Rithis\BECRussiaBundle\Entity\Tester,
+    Rithis\BECRussiaBundle\Form\Type\TesterType;
+
 /**
  * @Route("/test")
  */
 class TestController extends BaseController
 {
+    /**
+     * @Route("/register")
+     * @Method("GET|POST")
+     * @Template
+     */
+    public function registerAction()
+    {
+        $tester = $this->get('session')->get('rithis.becrussia.tester');
+        $form = $this->createForm(new TesterType(), $tester);
+        
+        $form->bind($this->getRequest());
+        
+        if ($form->isValid()) {
+            if ('POST' === $this->getRequest()->getMethod()) {
+                $this->get('session')->set('rithis.becrussia.tester', $form->getData());
+                
+                return $this->redirect($this->generateUrl('rithis_becrussia_test_questions'));
+            }
+        }
+
+        return array(
+            'form' => $form->createView(),
+        );
+    }
+
     /**
      * @Route("/questions")
      * @Method("GET")
@@ -18,6 +46,10 @@ class TestController extends BaseController
      */
     public function questionsAction()
     {
+        if (!$this->get('session')->has('rithis.becrussia.tester')) {
+            return $this->redirect($this->generateUrl('rithis_becrussia_test_register'));
+        }
+
         return array(
             'questions' => $this->getRepository('TestQuestion')->findAll(),
         );
@@ -37,6 +69,34 @@ class TestController extends BaseController
 
         return array(
             'result' => $result,
+        );
+    }
+
+    /**
+     * @Route("/submit")
+     * @Method("POST|GET")
+     * @Template
+     */
+    public function submitAction()
+    {
+        $tester = $this->get('session')->get('rithis.becrussia.tester');
+        $form = $this->createForm(new TesterType(), $tester);
+
+        $form->bind($this->getRequest());
+        
+        if ($form->isValid()) {
+            if ('POST' === $this->getRequest()->getMethod()) {
+                $em = $this->getDoctrine()->getManager();
+                
+                $em->persist($form->getData());
+                $em->flush();
+                
+                return $this->redirect($this->generateUrl('rithis_becrussia_test_questions'));
+            }
+        }
+
+        return array(
+            'form' => $form->createView(),
         );
     }
 }
